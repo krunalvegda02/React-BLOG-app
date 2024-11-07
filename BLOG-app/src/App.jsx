51d83092df67e4e97b48be4a1515d0f444201e80
krunalvegda02 <Krunal.vegda02@gmail.com>
2024-11-07 22:22:50 +0530

    AppWrite setup

--- BLOG-app/src/App.jsx
+++ BLOG-app/src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-
-    </>
-  )
+    <p className=" bg-black">
+      {console.log(import.meta.env.VITE_APPWRITE_URL)}
+      
+    </p>
+  );
 }
 
-export default App
+export default App;
