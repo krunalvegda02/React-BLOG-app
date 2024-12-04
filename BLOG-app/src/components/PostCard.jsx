49b1f37c8ec8a9fe230fa305eee6b0976b0da21c
krunalvegda02 <Krunal.vegda02@gmail.com>
2024-12-04 18:53:32 +0530

    UI change

--- BLOG-app/src/components/PostCard.jsx
+++ BLOG-app/src/components/PostCard.jsx
@@ -7,15 +7,15 @@ function PostCard({ $id, title, featured_image }) {
   
   return (
     <Link to={`/post/${$id}`}>
-      <div className={`w-full bg-gray-100 rounded-xl p-3 pb-0`}>
-        <div className="w-full justify-center mb-2 h-40">
+      <div className={`w-full bg-gray-100 rounded-xl p-3 py-3`}>
+        <div className="w-full justify-center mb-2 h-60">
           <img
             src={imagePreview}
             alt={title}
             className="rounded-xl h-full"
           />
         </div>
-        <h2 className="text-xl font-mono font-semibold  overflow-hidden">{title}</h2>
+        <h2 className="text-xl font-mono font-semibold  overflow-hidden ">{title}</h2>
       </div>
     </Link>
   );
